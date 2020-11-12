5d6359b03eeb4b7523107ae16e94522f49a3c604
Brendan McKenzie <hello@brendanmckenzie.com>
2020-11-12 19:06:00 +1100

    updated favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,10 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, maximum-scale=1"
         />
-        <link rel="icon" href="/favicon.svg" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
+        />
       </Head>
       <Component {...pageProps} />
     </>
